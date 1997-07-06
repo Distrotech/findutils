d19dbdf8fc66a0636704b2cbf69df6c16cf0dc78
Kevin Dalley <kevin@seti.org>
1997-07-06 21:04:42 +0000

    * add "#define _GNU_SOURCE" to pred.c

--- find/pred.c
+++ find/pred.c
@@ -15,6 +15,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+#define _GNU_SOURCE
 #include <config.h>
 #include <sys/types.h>
 #include <sys/stat.h>
