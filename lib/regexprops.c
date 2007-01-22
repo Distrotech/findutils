f198887c2eba4e979ddfdf0c248599462b359f96
James Youngman <jay@gnu.org>
2007-01-22 10:34:02 +0000

    Fixed typo

--- lib/regexprops.c
+++ lib/regexprops.c
@@ -306,7 +306,7 @@ describe_regex_syntax(int options)
       literal("The character @samp{$} only represents the end of a string when it appears:");
       beginenum();
       enum_item("At the end of a regular expression");
-      enum_item("Before an close-group, signified by ");
+      enum_item("Before a close-group, signified by ");
       if (options & RE_NO_BK_PARENS)
 	{
 	  literal("@samp{)}");
