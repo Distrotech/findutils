e3bcac430a07a05ba2ac66587d168a842ba8c0ea
Jim Meyering <meyering@redhat.com>
2008-12-26 18:28:10 +0100

    find: take advantage of new gnulib/fts leaf-optimization
    * find/ftsfind.c (consider_visiting): Allow state.type to be 0
    when fts_info is FTS_NSOK;
    
    This allows find to process an fts entry for which fts_read returns
    FTS_NSOK (no stat) but for which find requires only type info.
    This happens on file systems that lack dirent.dtype information.
    Currently, only reiserfs is handled this way.  Until the recent
    gnulib/fts change, [97d5b66] "fts: arrange not to stat non-directories
    in more cases" this change was not necessary, because fts would always
    stat non-dir entries on a file system with no dirent.dtype information.
    
    However, combined with the gnulib change, this change lets find
    avoid many per-non-directory stat-like syscalls (i.e. fstatat)
    in some very common cases, like "find . -print" on reiserfs --
    which can be a huge performance savings.

--- find/ftsfind.c
+++ find/ftsfind.c
@@ -1,6 +1,6 @@
 /* find -- search for files in a directory hierarchy (fts version)
    Copyright (C) 1990, 91, 92, 93, 94, 2000, 2003, 2004, 2005, 2006,
-                 2007, 2008 Free Software Foundation, Inc.
+                 2007, 2008, 2009 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -472,7 +472,7 @@ consider_visiting(FTS *p, FTSENT *ent)
       || ent->fts_info == FTS_NS /* e.g. symlink loop */)
     {
       assert (!state.have_stat);
-      assert (state.type != 0);
+      assert (ent->fts_info == FTS_NSOK || state.type != 0);
       mode = state.type;
     }
   else
