7d2abda8d4bf41591f18925693474b3cd8a3f783
James Youngman <jay@gnu.org>
2009-07-05 00:23:06 +0100

    Move regexprops.texi into git and check the autogenerated version is the same.
    * lib/check-regexprops.sh: New test.  Run regexprops as a check,
    and compare its output against the contents of doc/regexprops.texi.
    If there is a mismatch for regexprops.texi, save the correct version.
    * lib/Makefile.am: Run check-regexprops.sh for 'make check'.
    * doc/Makefile.am: don't clean regexprops.texi, and don't generate
    it automatically.
    * doc/regexprops.texi: add into source control.
    it automatically.
    * doc/.gitignore: don't ignore regexprops.texi

---
+++ lib/check-regexprops.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+# Generate regexprops.texi and compare it against the checked-in version.
+#
+existing=${srcdir}/../doc/regexprops.texi
+
+case ${REGEXPROPS} in
+    /*) ;;
+    *) REGEXPROPS="./${REGEXPROPS}"
+esac
+
+save_output="regexprops.texi.new"
+
+rv=1
+if output_file=`mktemp`
+then
+    ${REGEXPROPS} "Regular Expressions" findutils |
+    sed -e 's/[	 ]\+$//' >| "${output_file}"
+    if cmp "${existing}" "${output_file}" ; then
+	echo "${existing} is up to date."
+	rv=0
+    else
+	echo "${existing} is out of date." >&2
+	cp "${output_file}" "${save_output}" &&
+	  echo "Updated output is saved in ${save_output}" >&2
+	rv=1
+    fi
+    rm -f "${output_file}"
+fi
+exit "$rv"
