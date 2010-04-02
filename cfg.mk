efd6e25a4ba71a5a52cc6d81003abb405fa9917a
James Youngman <jay@gnu.org>
2010-04-02 18:46:24 +0100

    cfg.mk: simplify variables
    * cfg.mk: Append the names of checks to skip to
    local-checks-to-skip rather than inventing 7 new variables.
    
    Signed-off-by: James Youngman <jay@gnu.org>

--- cfg.mk
+++ cfg.mk
@@ -14,38 +14,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+local-checks-to-skip :=
+
 # Errors I think are too picky anyway.
-skip_too_picky = sc_error_message_period sc_error_message_uppercase \
+local-checks-to-skip += sc_error_message_period sc_error_message_uppercase \
 	sc_file_system
 
 # Errors I have not investigated; diagnose and fix later.
-skip_dunno = sc_immutable_NEWS sc_makefile_at_at_check
+local-checks-to-skip += sc_immutable_NEWS sc_makefile_at_at_check
 
 # False positives I don't have a workaround for yet.
 # sc_space_tab: several .xo test output files contain this sequence
 #               for testing xargs's handling of white space.
-false_positives = sc_obsolete_symbols sc_prohibit_cvs_keyword \
+local-checks-to-skip += sc_obsolete_symbols sc_prohibit_cvs_keyword \
 	sc_two_space_separator_in_usage \
 	sc_space_tab
 
 # Problems that have some false positives and some real ones; tease
 # apart later.
-mix_positives = sc_trailing_blank
+local-checks-to-skip += sc_trailing_blank
 
 # Problems partly fixed in other patches which aren't merged yet.
-skip_blocked_patch = sc_useless_cpp_parens
+local-checks-to-skip += sc_useless_cpp_parens
 
 # Problems we can't esaily fixed because they apply to files which we need
 # to keep in sync, so can't easily make a local change to.
 # sc_texinfo_acronym: perms.texi from coreutils uses @acronym{GNU}.
-skip_blocked_notours = \
+local-checks-to-skip += \
 	sc_texinfo_acronym
 
 # sc_prohibit_strcmp is broken because it gives false positives for cases
 # where neither argument is a string literal.
 # sc_prohibit_stat_st_blocks produces a false positive on definition of ST_NBLOCKS.
-skip_broken_checks = sc_prohibit_strcmp sc_prohibit_stat_st_blocks
-
-local-checks-to-skip = \
-	$(skip_too_picky) $(skip_dunno) $(false_positives) \
-	$(mix_positives) $(skip_blocked_patch) $(skip_blocked_notours) $(skip_broken_checks)
+local-checks-to-skip += sc_prohibit_strcmp sc_prohibit_stat_st_blocks
