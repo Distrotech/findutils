233477a764c7d122af83736715940b58072fdc65
James Youngman <jay@gnu.org>
2011-06-04 01:59:16 +0100

    More copyright year updates.
    * build-aux/check-testfiles.sh: Update copyright year.
    * m4/noreturn.m4: Update copyright year.
    * po/POTFILES.in: Update copyright year.

--- build-aux/check-testfiles.sh
+++ build-aux/check-testfiles.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # check-testfiles.sh -- Check we distributed all the test files we need
-# Copyright (C) 2007, 2010 Free Software Foundation, Inc.
+# Copyright (C) 2007, 2010, 2011 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
