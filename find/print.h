3953192b4e3e750e0795eae803524a67ad15e48f
James Youngman <jay@gnu.org>
2011-06-17 23:29:41 +0100

    Split find's printf-related code into a new file.
    * find/print.h: New file.  Declare insert_fprintf and
    make_segment.
    * find/print.c: New file. Move definitions of insert_fprintf and
    make_segment to here.
    * find/parser.c: Include "parser.h".  Move declarations of
    insert_fprintf and make_segment into that file; move the
    definitions into parser.c.
    * find/Makefile.am (libfindtools_a_SOURCES): Add print.c.
    (EXTRA_DIST): Add print.h.
    * po/POTFILES.in: Add find/print.c.

---
+++ find/print.h
@@ -0,0 +1,16 @@
+#include "defs.h"
+
+struct format_val;
+struct parser_table;
+struct predicate;
+struct segment;
+
+struct segment **make_segment (struct segment **segment,
+			       char *format, int len,
+			       int kind, char format_char,
+			       char aux_format_char,
+			       struct predicate *pred);
+bool
+insert_fprintf (struct format_val *vec,
+		const struct parser_table *entry, PRED_FUNC func,
+		const char *format_const);
